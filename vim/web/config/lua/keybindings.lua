1f6422a91b8c37bd5050edde15c9cc40ab7fdce4
wuchuheng <root@wuchuheng.com>
2022-07-02 14:06:34 +0800

    feat(wvim): updated config for keymaping.

--- vim/web/config/lua/keybindings.lua
+++ vim/web/config/lua/keybindings.lua
@@ -353,4 +353,44 @@ wk.register({
     },
 })
 
+-- TODO: quickfix快捷键功能,需要把vimscript转换lua
+-- pcall(
+--     vim.cmd,
+--     [[
+-- nnoremap <leader>g :set operatorfunc=<SID>GrepOperator<cr>g@
+-- vnoremap <leader>g :<c-u>call <SID>GrepOperator(visualmode())<cr>
+-- nnoremap <leader>q :call QuickfixToggle()<cr>
+--
+-- function! s:GrepOperator(type)
+--     let saved_unnamed_register = @@
+--
+--     if a:type ==# 'v'
+--         normal! `<v`>y
+--     elseif a:type ==# 'char'
+--         normal! `[v`]y
+--     else
+--         return
+--     endif
+--
+--     silent execute "grep! -R " . shellescape(@@) . " ."
+--     copen
+--
+--     let @@ = saved_unnamed_register
+-- endfunction
+--
+-- let g:quickfix_is_open = 0
+-- function! QuickfixToggle()
+--     if g:quickfix_is_open
+--         cclose
+--         let g:quickfix_is_open = 0
+--         execute g:quickfix_return_to_window . "wincmd w"
+--     else
+--         let g:quickfix_return_to_window = winnr()
+--         copen
+--         let g:quickfix_is_open = 1
+--     endif
+-- endfunction
+-- ]]
+-- )
+
 return pluginKeys
