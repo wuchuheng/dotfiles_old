7357d8a47ff965dcdad9a940b6ded43c4bbc137c
wuchuheng <root@wuchuheng.com>
2022-07-10 02:01:29 +0800

    feat(cvim): updated the config for flutter.

--- vim/cocvim/config/lua/plugin-configs/coc/config/flutter.lua
+++ vim/cocvim/config/lua/plugin-configs/coc/config/flutter.lua
@@ -2,7 +2,7 @@ local Module = {}
 local autoCmd = require('autocmd')
 
 function Module.loadConfig()
-    local extensionsName = 'coc-flutter'
+    local extensionsName = 'coc-flutter-tools'
     autoCmd.cocFlutter()
     return extensionsName
 end
