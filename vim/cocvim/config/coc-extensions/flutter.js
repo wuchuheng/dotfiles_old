5a35e2d9ec5936aed929eb828f3955d661dc4351
wuchuheng <root@wuchuheng.com>
2022-07-12 01:55:01 +0800

    feat(cvim): created the customer extension of coc for flutter .

---
+++ vim/cocvim/config/coc-extensions/flutter.js
@@ -0,0 +1,40 @@
+const { commands, workspace, window } = require('coc.nvim')
+
+const debounce = (callback, wait) => {
+    let timer = 0
+    return () => {
+        timer && clearTimeout(timer)
+        timer = setTimeout(() => {
+            callback()
+        }, wait)
+    }
+}
+
+
+exports.activate = context => {
+    let { nvim } = workspace
+    // context.subscriptions.push(commands.registerCommand('code.convertCodePoint', async () => {
+    //
+    //   let [pos, line] = await nvim.eval('[coc#util#cursor(), getline(".")]')
+    //   let curr = pos[1] == 0 ? '' : line.slice(pos[1], pos[1] + 1)
+    //   let code = curr.codePointAt(0)
+    //   let str = code.toString(16)
+    //   str = str.length == 4 ? str : '0'.repeat(4 - str.length) + str
+    //   let result = `${line.slice(0, pos[1])}${'\\u' + str}${line.slice(pos[1] + 1)}`
+    //   await nvim.call('setline', ['.', result])
+    //
+    // }))
+    const onTyping = debounce(() => {
+        nvim.command('w | CocCommand flutter.dev.hotReload')
+    }, 500)
+
+    workspace.onDidChangeTextDocument(async e => {
+        const {uri} = e.textDocument
+        if(uri && uri.length > 0) {
+           const [,extension]= uri.split('.')
+            extension === 'dart' && onTyping()
+        }
+    }, null, [])
+}
+
+
--- vim/cocvim/config/init.lua
+++ vim/cocvim/config/init.lua
@@ -1,2 +1,6 @@
 require('lua.init')
+-- vim.cmd([[
+--     set runtimepath^=/Users/wuchuheng/Desktop/myprojects/coc/tmp/coc-git
+--     let g:coc_node_args = ['--nolazy', '--inspect=6045', '-r', expand('~/.config/yarn/global/node_modules/source-map-support/register')]
+-- ]])
 
