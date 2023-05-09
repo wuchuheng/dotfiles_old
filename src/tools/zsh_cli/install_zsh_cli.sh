1033ba337f1df37019940a14162f109f0019c763
wuchuheng <root@wuchuheng.com>
2023-05-09 22:48:41 +0800

    chore(github-action): update .github/workflows/dev.yml #11

--- src/tools/zsh_cli/install_zsh_cli.sh
+++ src/tools/zsh_cli/install_zsh_cli.sh
@@ -4,8 +4,8 @@
 os_type=$(uname)
 
 # 判断操作系统类型
-if [ "$os_type" = "debian" ] || [ "$os_type" = "ubuntu" ]; then
-    echo "Debian/Ubuntu"
+if [ "$os_type" = "Linux" ]; then
+    cat /etc/os-release
 elif [ "$os_type" = "centos" ] || [ "$os_type" = "rhel" ]; then
     echo "CentOS/RHEL"
 elif [ "$os_type" = "Darwin" ]; then
