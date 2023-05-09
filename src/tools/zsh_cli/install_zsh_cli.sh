e1a26fdec6cc07752fa6260883c031d86faf0538
wuchuheng <root@wuchuheng.com>
2023-05-09 22:51:02 +0800

    chore(github-action): update .github/workflows/dev.yml #12

--- src/tools/zsh_cli/install_zsh_cli.sh
+++ src/tools/zsh_cli/install_zsh_cli.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 # 检测当前操作系统类型
-os_type=$(uname)
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    os_type=$ID
+else
+    os_type=$(uname)
+fi
 
 # 判断操作系统类型
-if [ "$os_type" = "Linux" ]; then
-    cat /etc/os-release
+if [ "$os_type" = "debian" ] || [ "$os_type" = "ubuntu" ]; then
+    echo "Debian/Ubuntu"
 elif [ "$os_type" = "centos" ] || [ "$os_type" = "rhel" ]; then
     echo "CentOS/RHEL"
 elif [ "$os_type" = "Darwin" ]; then
