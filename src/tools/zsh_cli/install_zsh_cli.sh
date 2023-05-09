c65af7878ad6da506e3dc6c49dd92a3f30c8069f
wuchuheng <root@wuchuheng.com>
2023-05-10 03:12:11 +0800

    chore(github-action): update .github/workflows/dev.yml #32

--- src/tools/zsh_cli/install_zsh_cli.sh
+++ src/tools/zsh_cli/install_zsh_cli.sh
@@ -25,13 +25,6 @@ successLog() {
 if [ "$OS_TYPE" = "debian" ] || [ "$OS_TYPE" = "ubuntu" ]; then
     sudo apt update && sudo apt install zsh -y && successLog
 elif [ "$OS_TYPE" = "centos" ] || [ "$OS_TYPE" = "rhel" ]; then
-    # CentOS 8 
-    minorver=8.5.2111
-    sed -e "s|^mirrorlist=|#mirrorlist=|g" \
-             -e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=https://mirrors.aliyun.com/centos-vault/$minorver|g" \
-             -i.bak \
-         /etc/yum.repos.d/CentOS-*.repo
-    yum makecache    
     yum update -y && yum install -y zsh && successLog
 elif [ "$OS_TYPE" = "Darwin" ]; then
     brew install zsh && successLog;
