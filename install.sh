a732f3798ee440bd578469590a4035acd816c64d
wuchuheng <root@wuchuheng.com>
2023-05-09 21:47:07 +0800

    feat: add git in tools dirctory

--- install.sh
+++ install.sh
@@ -58,4 +58,5 @@ done
 
 bash $PROJECT_PATH/src/checkers/zshChecker.sh || exit 1
 bash $PROJECT_PATH/src/tools/send_email_notification_cli/install_check_send_email_notifcation_cli.sh || exit 1
+bash $PROJECT_PATH/src/tools/git_cli/check_is_exist_git_cli.sh || exit 1
 
