705b2a802ac251452600dc2d0435d60cf8ba2506
wuchuheng <root@wuchuheng.com>
2023-06-07 15:32:35 +0800

    refactory: optimized test library

---
+++ src/utils/autoload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+import() {
+  local RED='\033[0;31m'
+  local NC='\033[0m'
+  local timestamp=$(date +"%Y-%m-%d %T")
+  source_file=$DOTFILES_BASE_PATH$1
+  source "$source_file";
+  if [ $? != 0 ]; then
+    IFS=' ' read -r -a parts <<< $(caller 0)
+    printf "${RED}✖ ${timestamp} %s: line %s: %s ${NC} \n" ${parts[2]} ${parts[0]} "Failed to load $source_file, the file not found."
+    exit 1
+  fi
+}
+
---
+++ src/utils/test_except.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##
+# 断言字符
+# except_string "excepted str" "received str"
+##
+except_str() {
+  local prefix_path_len=${#DOTFILES_BASE_PATH}
+  ((prefix_path_len++))
+  local test_file=${BASH_SOURCE[1]}
+  current_file=${test_file:${prefix_path_len}}
+  local line="${BASH_LINENO[0]}"
+  local is_ok=0
+  if [ "$1" == "$2" ];then
+    is_ok=1
+  fi
+  test_name="test_name:${TEST_NAME}"
+  if [ ${is_ok} == 1 ];then
+    # printf "$(bg_green_print " PASS ") ${current_file}:${line} $test_name \n"
+    return 0
+  else
+    printf "$(bg_red_print " FAIL ") ${current_file}:${line}\n"
+    printf "    $(pink_print '●') $(pink_print "${TEST_NAME}")\n"
+    printf "    Expected: $(green_print "$1")\n"
+    printf "    Received: $(red_print "$2")\n\n"
+    global_is_pass=1
+    return 1
+  fi
+}
