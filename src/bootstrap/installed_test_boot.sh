8abedc66a3b31412725b220488d3d989983f5806
wuchuheng <root@wuchuheng.com>
2023-06-14 22:28:04 +0800

    refactory(installed_test):simplify the code to bootstrap installed tests

--- src/bootstrap/installed_test_boot.sh
+++ src/bootstrap/installed_test_boot.sh
@@ -1,9 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 DOTFILES_BASE_PATH=$(pwd)
-source ${DOTFILES_BASE_PATH}/src/utils/autoload.sh || exit 1 
+source ${DOTFILES_BASE_PATH}/src/utils/autoload.sh || exit 1
 
-import @/src/utils/color_printf.sh
+import @/src/handlers/test_handler.sh
 import @/src/utils/helper.sh
 
-# test_by_installed_state "installed"
+all_test_dir=(
+src/cli/1_zsh_cli
+)
+
+all_test_files=()
+for test_dir in ${all_test_dir[@]}; do
+  all_test_files+=($(get_test_files "${test_dir}" 'installed_tests'))
+done
+
+test_handle "${all_test_files}"
 
