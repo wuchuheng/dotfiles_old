dd2de51558ff29d17520903c5daeff8c5726bb90
wuchuheng <root@wuchuheng.com>
2023-06-18 22:28:31 +0800

    refactory(unistalled-test): update the bootstrap for the uninstalled tests

--- src/bootstrap/uninstalled_test_boot.sh
+++ src/bootstrap/uninstalled_test_boot.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-
 DOTFILES_BASE_PATH=$(pwd)
-source $DOTFILES_BASE_PATH/src/utils/helper.sh || exit 1
+source ${DOTFILES_BASE_PATH}/src/utils/autoload.sh || exit 1
 
-test_by_installed_state "uninstalled"
+import @/src/handlers/test_handler.sh
+import @/src/utils/helper.sh
+import @/src/config/test_conf.sh
+
+all_test_files=()
+for test_dir in ${ALL_TEST_DIR[@]}; do
+  all_test_files+=($(get_test_files "${test_dir}" 'uninstalled_tests'))
+done
+
+test_handle "${all_test_files}"
 
