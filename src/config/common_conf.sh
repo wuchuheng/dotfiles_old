c46f8ea616de560a142ad39e97f03ebc5b2abb17
wuchuheng <root@wuchuheng.com>
2023-06-23 10:54:31 +0800

    feat: update src/config/common_conf.sh #1

--- src/config/common_conf.sh
+++ src/config/common_conf.sh
@@ -2,3 +2,14 @@
 
 DOTFILES_CLI_PATH=src/cli
 
+GLOBAL_OS=''
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  OS=$(cat /etc/os-release | grep ID= | awk 'NR == 1 {print $1}' | sed -n 's/ID="\([^"]*\)".*/\1/p')
+  echo $OS
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  GLOBAL_OS="MacOS"
+else
+  echo "Unknown OS: $OSTYPE"
+  exit 1
+fi
