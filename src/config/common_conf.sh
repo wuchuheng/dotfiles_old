d9c91c028fd2f09d001300992dba8847cc0f806a
wuchuheng <root@wuchuheng.com>
2023-06-23 12:33:59 +0800

    feat: update src/config/common_conf.sh #4

--- src/config/common_conf.sh
+++ src/config/common_conf.sh
@@ -6,10 +6,23 @@ GLOBAL_OS=''
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   OS=$(cat /etc/os-release | grep NAME= | awk 'NR == 1 {print $0}' |  sed -n 's/NAME="\([^"]*\)".*/\1/p')
-  echo $OS
+  case ${OS} in
+    'Ubuntu')
+        GLOBAL_OS='Ubuntu';
+    ;;
+    'CentOS Linux')
+        GLOBAL_OS='CentOS Linux';
+    ;;
+    *)
+        echo "Unknown OS: $OSTYPE"
+	exit 1
+    ;;
+  esac
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   GLOBAL_OS="MacOS"
 else
   echo "Unknown OS: $OSTYPE"
   exit 1
 fi
+
+echo $GLOBAL_OS
