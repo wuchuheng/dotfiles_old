e39aa5cf5d1b292d02648a572f5900815d96f03b
wuchuheng <root@wuchuheng.com>
2023-06-27 11:21:25 +0800

    fix: fix the issue that no loading all the installation provider for cli

--- src/common/install/install_all_cli.sh
+++ src/common/install/install_all_cli.sh
@@ -11,7 +11,7 @@ function to_install_all_cli() {
   to_load_all_cli_template=${DOTFILES_BASE_PATH}/src/common/install/to_load_all_cli_template.sh
   cat ${to_load_all_cli_template} >> ${main_sh_path}
   local all_cli=($(get_all_sub_dir_by_path "${DOTFILES_BASE_PATH}/${DOTFILES_CLI_PATH}"))
-  for cli_name in ${all_cli}; do
+  for cli_name in ${all_cli[@]}; do
     local cli_number=$(get_cli_number_by_cli_name_with_number "${cli_name}")
     local cli_name_without_number=$(get_cli_name_by_cli_name_with_number "${cli_name}")
     local profider=${DOTFILES_CLI_PATH}/${cli_name}/${cli_name_without_number}_installation_provider/${cli_name_without_number}_installation_provider.sh
