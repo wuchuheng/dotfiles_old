7c265e95742499c728ff9677d8489f7d9711b3d5
wuchuheng <root@wuchuheng.com>
2023-06-27 14:02:53 +0800

    test(installed_test): add a test to check if zsh exist

--- src/cli/3_zsh_cli/__test__/installed_tests/1_zsh.test.sh
+++ src/cli/3_zsh_cli/__test__/installed_tests/1_zsh.test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+import @/src/utils/helper.sh
+import @/src/utils/test_except.sh
+
 # zsh cli test.
 function zsh_installed_test() {
-    echo "zsh test after installed."
+  local is_command_exist='false'
+  if is_command 'zsh'; then
+    is_command_exist='true'
+  fi
+  except_str 'true' "${is_command_exist}"
 }
 
 handle_testing_callback "zsh_installed_test" "the integration test of the zsh cli after the dotfiles installed."
