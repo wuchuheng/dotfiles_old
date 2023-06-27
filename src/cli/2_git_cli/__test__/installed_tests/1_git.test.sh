797dc0b4219ee2e207ec7d667d7177cb6abf8a37
wuchuheng <root@wuchuheng.com>
2023-06-27 13:53:55 +0800

    fix(test):fix the issue that was not handle all test files after the project installed.

--- src/cli/2_git_cli/__test__/installed_tests/1_git.test.sh
+++ src/cli/2_git_cli/__test__/installed_tests/1_git.test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+import @/src/utils/helper.sh
+import @/src/utils/test_except.sh
+
 # git cli test.
 function git_installed_test() {
-    echo "git test after installed."
+  local is_command_exist='false'
+  if is_command 'git'; then
+    is_command_exist='true'
+  fi
+  except_str 'true' "${is_command_exist}"
 }
 
 handle_testing_callback "git_installed_test" "the integration test of the git cli after the dotfiles installed."
