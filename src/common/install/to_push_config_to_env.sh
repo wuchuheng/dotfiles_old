67feea846ecd0ca198222e0fa0d995a26b2d6243
wuchuheng <root@wuchuheng.com>
2023-06-27 09:56:59 +0800

    fix: fix the issue that the OS name not found in unbuntu #8

--- src/common/install/to_push_config_to_env.sh
+++ src/common/install/to_push_config_to_env.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
-# Add bootstrap configuration to .bashrc.
-if [ -f ~/.bashrc ]; then
-  TAG_FOR_BASH="$(get_install_start_symbol_in_zshrc)"
-  isloadBashrcToBashrc=$(cat ~/.bashrc | grep "$TAG_FOR_BASH")
-  if [ "$isloadBashrcToBashrc" == '' ]; then
-      echo "To change bashrc"
-      echo "$(get_install_start_symbol_in_zshrc)" >> ~/.bashrc
-      echo "zsh" >> ~/.bashrc
-      echo "$(get_install_end_symbol_in_zshrc)" >> ~/.bashrc
+function to_push_config_to_env() {
+  # Add bootstrap configuration to .bashrc.
+  if [ -f ~/.bashrc ]; then
+    TAG_FOR_BASH="$(get_install_start_symbol_in_zshrc)"
+    isloadBashrcToBashrc=$(cat ~/.bashrc | grep "$TAG_FOR_BASH")
+    if [ "$isloadBashrcToBashrc" == '' ]; then
+        echo "To change bashrc"
+        echo "$(get_install_start_symbol_in_zshrc)" >> ~/.bashrc
+        echo "zsh" >> ~/.bashrc
+        echo "$(get_install_end_symbol_in_zshrc)" >> ~/.bashrc
+    fi
   fi
-fi
-
-# Add bootstrap configuration to .zshrc.
-readonly local MAIN_FILE_PATH="$(get_full_path $(get_main_sh_path))"
-isloadBashrc=$(cat ~/.zshrc | grep "$MAIN_FILE_PATH")
-
-if [ "$isloadBashrc" == '' ]; then
-  log "INFO" "To load the main.sh to ~/.zshrc"
-  echo "$(get_install_start_symbol_in_zshrc)" >>~/.zshrc 
-  echo "if [ -e "$MAIN_FILE_PATH" ];then" >>~/.zshrc
-  echo "    source $MAIN_FILE_PATH;" >>~/.zshrc
-  echo "fi" >>~/.zshrc
-  echo "$(get_install_end_symbol_in_zshrc)" >>~/.zshrc 
-fi
+  
+  # Add bootstrap configuration to .zshrc.
+  readonly local MAIN_FILE_PATH="$(get_full_path $(get_main_sh_path))"
+  if [ ! -f ~/.zshrc ]; then
+    touch ~/.zshrc
+  fi
+  
+  isloadBashrc=$(cat ~/.zshrc | grep "$MAIN_FILE_PATH")
+  
+  if [ "$isloadBashrc" == '' ]; then
+    log "INFO" "To load the main.sh to ~/.zshrc"
+    echo "$(get_install_start_symbol_in_zshrc)" >>~/.zshrc 
+    echo "if [ -e "$MAIN_FILE_PATH" ];then" >>~/.zshrc
+    echo "    source $MAIN_FILE_PATH;" >>~/.zshrc
+    echo "fi" >>~/.zshrc
+    echo "$(get_install_end_symbol_in_zshrc)" >>~/.zshrc 
+  fi
+}
